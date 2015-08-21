41b6aff3a642e25e3ba542feaa3bcbfbf64019a0
Kenneth Li <kloong87i@gmail.com>
2015-08-21 00:50:37 -0700

    adding devourer to whitelist

--- data/riot_api/items.py
+++ data/riot_api/items.py
@@ -7,6 +7,10 @@ class RiotItems(object):
     3117, 3009, 3006, 3158, 3047, 3020, 3111, # boots
     3003, # archangels
     3004, # muamane
+    3710, # devourers
+    3718,
+    3722,
+    3726
   ]
 
   _FINAL_ITEM_BLACKLIST = [ # trinkets
