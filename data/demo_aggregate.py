f1e4725af96138c79225b94304c1b3740f9eb757
Kenneth Li <kloong87i@gmail.com>
2015-08-21 12:11:57 -0700

    adding demo aggregate

---
+++ data/demo_aggregate.py
@@ -0,0 +1,62 @@
+from util import datetime_to_timestamp
+
+import sys, argparse, threading, time
+from pymongo import MongoClient
+from bson import ObjectId
+from bson.code import Code
+
+MAP_FN = """
+function() {
+  value = this.value
+  if (value.finalBuild.length < 4) {
+    return
+  }
+  var finalBuild = value.finalBuild
+  var sorted = value.finalBuild.slice().sort()
+  var key = {
+    championId: this._id.championId,
+    role: this._id.role,
+    lane: this._id.lane,
+    buildKey: sorted.join(",")
+  };
+
+  this._id = key;
+  value.sortedBuild = sorted
+  emit(key, this.value);
+}
+"""
+
+REDUCE_FN = """
+function(key, values) {
+  result = values[0];
+  for (var i=1; i < values.length; i++) {
+    result.buildIds.join(values[i].buildIds)
+    for (var key in result.stats) {
+      result.stats[key] = result.stats[key] + values[i].stats[key];
+    }
+  }
+  return result;
+}
+"""
+
+FINALIZE_FN = """
+function(key, reducedValue) {
+  reducedValue.winRate = reducedValue.stats.wins / reducedValue.stats.count;
+  return result;
+}
+"""
+
+def main(argv):
+  mongo_url = "mongodb://localhost:27017"
+
+  # Initialize MongoDB
+  mongo_client = MongoClient(mongo_url)
+  outliers_db = mongo_client.outliers
+
+  input_coll = outliers_db["build_stats"]
+
+  input_coll.map_reduce(MAP_FN, REDUCE_FN, "demo_build_stats", finalize=FINALIZE_FN, reduce_output=True)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
